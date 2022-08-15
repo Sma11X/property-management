69351f16de175083ee29f4e749b67502a78820eb
FXX <540351143@qq.com>
2022-08-15 22:20:35 +0800

    project init

---
+++ src/views/login/LoginView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login">
+    dengluye
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+  .login{
+    background-color: blue;
+    height: 100%;
+  }
+  html,body,#app{
+    height: 100%;
+  }
+</style>
